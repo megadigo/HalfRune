f5f0e6d20ecc5009cc586278d92103d6d6b09c91
Rodrigo Cordeiro <megadigo@gmail.com>
2012-05-09 14:33:44 +0100

    Added the new direction and stance concept to the avatar.

--- PlayerEntity.js
+++ PlayerEntity.js
@@ -1,6 +1,9 @@
 /*------------------- 
 a player entity
 -------------------------------- */
+var direction = "left";
+var stance = "normal";
+
 var PlayerEntity = me.ObjectEntity.extend({
  
     /* -----
@@ -8,7 +11,7 @@ var PlayerEntity = me.ObjectEntity.extend({
     constructor
  
     ------ */
- 
+   
     init: function(x, y, settings) {
     // call the constructor
     this.parent(x, y, settings);
@@ -18,11 +21,11 @@ var PlayerEntity = me.ObjectEntity.extend({
     this.gravity=0
     
     //animation
-    this.addAnimation("up",[0,1,2,3]);
-    this.addAnimation("right",[4,5,6,7]);
-    this.addAnimation("down",[8,9,10,11]);
-    this.addAnimation("left",[12,13,14,15]);
-    this.setCurrentAnimation("left");
+    this.addAnimation("normal_up",[0,1,2,3]);
+    this.addAnimation("normal_right",[4,5,6,7]);
+    this.addAnimation("normal_down",[8,9,10,11]);
+    this.addAnimation("normal_left",[12,13,14,15]);
+    this.setCurrentAnimation(stance + "_" + direction);
     
     // adjust the bounding box x,w,y,h
     this.updateColRect(2,12 , 2, 5);
@@ -37,72 +40,57 @@ var PlayerEntity = me.ObjectEntity.extend({
     update the player pos
  
     ------ */
-   update: function() {
- 
-        if (me.input.isKeyPressed('left')) {
-            this.doWalkHorizontal(true);
-        } else if (me.input.isKeyPressed('right')) {
-            this.doWalkHorizontal(false);
-	} else if (me.input.isKeyPressed('up')) {
-            this.doWalkVertical(true);
-	} else if (me.input.isKeyPressed('down')) {
-            this.doWalkVertical(false);
-        } else {
-            this.vel.x = 0;
-	    this.vel.y = 0;
-        }
-        
-     
-        // check & update player movement
-        this.updateMovement();
-     
-        // check for collision
-        /*var res = me.game.collide(this);
-     
-        if (res) {
-            // if we collide with an enemy
-            if (res.obj.type == me.game.ENEMY_OBJECT) {
-                // check if we jumped on it
-                if ((res.y > 0) && ! this.jumping) {
-                    // bounce
-                    me.audio.play("stomp");
-                    this.forceJump();
-                } else {
-                    // let's flicker in case we touched an enemy
-                    this.flicker(45);
-                    me.game.HUD.setItemValue("score", 0);
-                }
-            }
-        }
-	*/
-        // update animation if necessary
-        if (this.vel.x!=0 || this.vel.y!=0) {
-            // update objet animation
-	if (this.vel.x > 0) {
-	    this.setCurrentAnimation("right")
-	};
-	if (this.vel.x < 0) {
-	    this.setCurrentAnimation("left")
-	};
-	if (this.vel.y > 0) {
-	    this.setCurrentAnimation("down")
-	};
-	if (this.vel.y < 0) {
-	    this.setCurrentAnimation("up")
-	};
+   update: function() { 
+		
+		// Handle Inputs
+		if (me.input.isKeyPressed('left')) {
+		    this.doWalkHorizontal(true);
+		} else if (me.input.isKeyPressed('right')) {
+		    this.doWalkHorizontal(false);
+		} else if (me.input.isKeyPressed('up')) {
+			this.doWalkVertical(true);
+		} else if (me.input.isKeyPressed('down')) {
+			this.doWalkVertical(false);
+		} else {
+		    this.doStand();
+		}
+		
+		// check & update player movement
+		this.updateMovement();
+  		
+  		// update animation if necessary
+	    if (this.vel.x!=0 || this.vel.y!=0) {
+			if (this.vel.x > 0) {
+			    direction="right";
+			};
+			if (this.vel.x < 0) {
+			    direction="left";
+			};
+			if (this.vel.y > 0) {
+			    direction="down";
+			};
+			if (this.vel.y < 0) {
+			    direction="up";
+			};
+            this.setCurrentAnimation(stance + "_" + direction);
             this.parent(this);
             return true;
-        }
-        return false;
+	   	} else {
+		 return false;
+		} 
     },
- 
+	 
     doWalkHorizontal : function(left) {
-	
-	this.vel.x += (left) ? -this.accel.x * me.timer.tick : this.accel.x * me.timer.tick;
+		this.vel.x += (left) ? -this.accel.x * me.timer.tick : this.accel.x * me.timer.tick;
     },
+    
     doWalkVertical : function(up) {
-	
-	this.vel.y += (up) ? -this.accel.y * me.timer.tick : this.accel.y * me.timer.tick;
+		this.vel.y += (up) ? -this.accel.y * me.timer.tick : this.accel.y * me.timer.tick;
+    },
+    
+    doStand : function(up) {
+		this.vel.y = 0;
+		this.vel.x = 0;
     }
 }
 );
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Porta it's my life project. I have all my life to finish it. Its suppose to be a
 Based in an simple question: What if instead of fire, man discover a Portal, that allow him to 
 travel to when ever he wish?
 
-This is the starting point for a big adventure.
+This is the starting point for a big adventure
 
 Try it at http://porta.tapjs.com
 
@@ -13,14 +13,11 @@ To do's:
 ========
 
 - Choose the framework to do the game - MelonJs(https://github.com/obiot/melonJS) Done
-- Find some free sprites - Well not free i think, u6tiles from ultima 6. Just for now, till i get one better Done
+- Find some free sprites - Well not free i think, u6tiles from ultima 6. Just for know, till i get one better Done
 - Add some basic test features;
   - Publish to GitHub Done
   - Add some backgroung elements walls, floor Done
   - Add the Avatar with proper animation Done
-  - Implement the tap/mouse movememt;
-  - Add A sword to the Avatar
-  - Add some enemies;
   - Add some Entity to interact;
   - Add quest basic;
 - Write the history;
